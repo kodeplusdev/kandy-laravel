482d7e8f3ef9ba74f13b49efcc180a98e4722fc5
kodeplusdev <kodeplusdev@gmail.com>
2014-12-10 14:27:48 +0700

    Initial project

---
+++ src/Kodeplusdev/Kandylaravel/KandylaravelServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Kodeplusdev\Kandylaravel;
+
+use Illuminate\Support\ServiceProvider;
+
+class KandylaravelServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('kodeplusdev/kandylaravel');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
