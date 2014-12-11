c1c9971a01aa3d582e2f16a80ec1eea75f37c9ac
kodeplusdev <kodeplusdev@gmail.com>
2014-12-11 12:06:45 +0700

    Added create_users_table' migration.

---
+++ src/migrations/2014_12_11_044619_create_users_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUsersTable extends Migration
+{
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $tableName = \Config::get("kandylaravel::user_table");
+        $userIdColumn = \Config::get("kandylaravel::user_id_column");
+        $passwordColumn = \Config::get("kandylaravel::password_column");
+
+        Schema::create(
+            $tableName,
+            function (Blueprint $table) use ($userIdColumn, $passwordColumn) {
+                $table->increments('id');
+                $table->string($userIdColumn);
+                $table->string($passwordColumn);
+                $table->string('main_user_id');
+            }
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+
+}
