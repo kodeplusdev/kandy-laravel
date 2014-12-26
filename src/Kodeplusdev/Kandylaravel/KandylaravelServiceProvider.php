cffaef5ad6ef88ee5c13d64e87baf0fb140c462f
kodeplusdev <kodeplusdev@gmail.com>
2014-12-26 15:23:00 +0700

    register publish assets

--- src/Kodeplusdev/Kandylaravel/KandylaravelServiceProvider.php
+++ src/Kodeplusdev/Kandylaravel/KandylaravelServiceProvider.php
@@ -45,14 +45,7 @@ class KandylaravelServiceProvider extends ServiceProvider
 
         $this->registerChat();
         $this->app['artisan']->call("asset:publish", array("kodeplusdev/kandylaravel"));
-       // $this->publishAssets();
-    }
 
-    /**
-     * Auto publish assets when update core
-     */
-    public function publishAssets(){
-        $this->app['artisan']->call("asset:publish", array("kodeplusdev/kandylaravel"));
     }
 
     /**
