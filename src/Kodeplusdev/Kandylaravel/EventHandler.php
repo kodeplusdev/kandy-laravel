b54c93d722dad9c46106c634a0e7db1e99e9738b
khanhht <khanhphp@gmail.com>
2015-08-06 12:09:39 +0700

    fix update user status ajax error, user not assigned error, add addition condition to livechat::getFreeUser function

--- src/Kodeplusdev/Kandylaravel/EventHandler.php
+++ src/Kodeplusdev/Kandylaravel/EventHandler.php
@@ -14,38 +14,43 @@ class EventHandler {
     {
         $kandyUser = KandyUsers::where('main_user_id', $user->id)->first();
         //if login user is a chat agent
-        if($kandyUser->type == Kandylaravel::USER_TYPE_CHAT_AGENT) {
-            $model = KandyUserLogin::where('kandy_user_id', $kandyUser->user_id)->first();
-            $remoteIp = $_SERVER['REMOTE_ADDR'];
-            $now = time();
-            if(!$model) {
-                KandyUserLogin::create(array(
-                    'kandy_user_id' => $kandyUser->user_id,
-                    'type' => Kandylaravel::USER_TYPE_CHAT_AGENT,
-                    'status' => Kandylaravel::USER_STATUS_ONLINE,
-                    'browser_agent' => $_SERVER["HTTP_USER_AGENT"],
-                    'ip_address' => $remoteIp,
-                    'time'      => $now,
-                ));
-            } else {
-                $model->update(array(
-                    'status'    => Kandylaravel::USER_STATUS_ONLINE,
-                    'time'      => $now,
-                    'ip_address'=> $remoteIp,
-                    'browser_agent' => $_SERVER["HTTP_USER_AGENT"],
-                ));
+        if($kandyUser) {
+            if($kandyUser->type == Kandylaravel::USER_TYPE_CHAT_AGENT) {
+                $model = KandyUserLogin::where('kandy_user_id', $kandyUser->user_id)->first();
+                $remoteIp = $_SERVER['REMOTE_ADDR'];
+                $now = time();
+                if(!$model) {
+                    KandyUserLogin::create(array(
+                        'kandy_user_id' => $kandyUser->user_id,
+                        'type' => Kandylaravel::USER_TYPE_CHAT_AGENT,
+                        'status' => Kandylaravel::USER_STATUS_ONLINE,
+                        'browser_agent' => $_SERVER["HTTP_USER_AGENT"],
+                        'ip_address' => $remoteIp,
+                        'time'      => $now,
+                    ));
+                } else {
+                    $model->update(array(
+                        'status'    => Kandylaravel::USER_STATUS_ONLINE,
+                        'time'      => $now,
+                        'ip_address'=> $remoteIp,
+                        'browser_agent' => $_SERVER["HTTP_USER_AGENT"],
+                    ));
+                }
             }
         }
+
         
     }
 
     public function onUserLogout($user){
         $kandyUser = KandyUsers::where('main_user_id', $user->id)->first();
         //if login user is a chat agent
-        if($kandyUser->type == Kandylaravel::USER_TYPE_CHAT_AGENT) {
-            $userLogin = KandyUserLogin::where('kandy_user_id', $kandyUser->user_id)->first();
-            $userLogin->status = Kandylaravel::USER_STATUS_OFFLINE;
-            $userLogin->save();
+        if($kandyUser) {
+            if($kandyUser->type == Kandylaravel::USER_TYPE_CHAT_AGENT) {
+              $userLogin = KandyUserLogin::where('kandy_user_id', $kandyUser->user_id)->first();
+              $userLogin->status = Kandylaravel::USER_STATUS_OFFLINE;
+              $userLogin->save();
+            }
         }
     }
 
