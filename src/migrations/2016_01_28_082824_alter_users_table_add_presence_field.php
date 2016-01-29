217e4eee557a2f268a95b7d717362be4c6e8884a
Luan To <luantt1111@gmail.com>
2016-01-29 15:03:01 +0700

    Fix six issues in comment

--- src/migrations/2016_01_28_082824_alter_users_table_add_presence_field.php
+++ src/migrations/2016_01_28_082824_alter_users_table_add_presence_field.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterUsersTable extends Migration {
+class AlterUsersTableAddPresenceField extends Migration {
 
 	/**
 	 * Run the migrations.
